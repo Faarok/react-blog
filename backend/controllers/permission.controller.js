7de25ea223b76ad514c702a35e4054497d3f00c4
Svein SAMSON <ssamson@cabinet-bedin-immobilier.com>
2024-12-17 17:59:27 +0100

    - Permissions, controller, DB : Ajout des méthodes create et getByFilters

--- backend/controllers/permission.controller.js
+++ backend/controllers/permission.controller.js
@@ -1,10 +1,55 @@
 import permissionDb from '../databases/permission.db.js';
+import { tools, permissionRange } from '../functions.js';
+import fs from 'fs';
 
 const permissionController = {
     createPermission: async (req, res) => {
-        let { label, slug } = req.body;
+        let { scope, method, range } = req.body;
 
-        return console.log(label, slug);
+        if(tools.isStringEmpty(scope))
+            return res.status(400).json({ error: 'Empty permission scope' });
+
+        if(!fs.existsSync(`./controllers/${scope}.controller.js`))
+            return res.status(400).json({ error: 'Invalid scope' });
+
+        if(tools.isStringEmpty(method))
+            return res.status(400).json({ error: 'Empty permission method' });
+
+        if(tools.isStringEmpty(range))
+            return res.status(400).json({ error: 'Empty permission range' });
+
+        if(!Object.values(permissionRange).includes(range))
+            return res.status(400).json({ error: 'Invalid range' });
+
+        let selectData = [
+            { column: 'permission_scope:=', value: scope },
+            { logic: 'AND' },
+            { column: 'permission_method:=', value: method },
+            { logic: 'AND' },
+            { column: 'permission_range:=', value: range }
+        ];
+
+        let permissionExist = await permissionDb.getPermissionByFilter(selectData);
+        if(permissionExist.error)
+            return res.status(400).json({ error: permissionExist.error });
+
+        if(permissionExist.length !== 0)
+            return res.status(409).json({ error: 'Permission already exist' });
+
+        let insertData = {
+            columns: ['permission_scope', 'permission_method', 'permission_range'],
+            values: [
+                [scope, method, range]
+            ]
+        };
+
+        let insertResponse = await permissionDb.createPermission(insertData);
+        let responseError = insertResponse.error;
+
+        if(responseError)
+            return res.status(400).json({ error: responseError });
+
+        return res.status(201).json({ success: 'Permission created' });
     }
 }
 
--- backend/databases/permission.db.js
+++ backend/databases/permission.db.js
@@ -1,7 +1,18 @@
 import db from './init.db.js';
 
 const permissionDb = {
+    tableName: 'permission',
 
+    createPermission: async function(permissionData) {
+        let builder = db.queryBuilder.insert(this.tableName, permissionData).build();
+        let [results] = await db.poolQuery(builder.query, builder.values);
+        return results;
+    },
+    getPermissionByFilter: async function(selectData) {
+        let builder = db.queryBuilder.select(this.tableName).where(selectData).build();
+        let [results] = await db.poolQuery(builder.query, builder.values);
+        return results;
+    }
 }
 
 export default permissionDb;
